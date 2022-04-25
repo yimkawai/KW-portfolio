0e9808544e1c7b7297b517286404c6377888d5c3
kw <yimkawaii@gmail.com>
2022-04-25 04:17:03 -0400

    added grid.

---
+++ src/main.js
@@ -0,0 +1,8 @@
+function makeMarquee() {
+    const title = "keep it simple stupid";
+    const marqueeText = new Array(150).fill(title).join('&ensp; • &ensp;');
+    const marquee = document.querySelector('.marquee span');
+    marquee.innerHTML = marqueeText;
+}
+
+makeMarquee();
