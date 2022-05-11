6a0299faaf8a1fea25b2706b19aabcb1e887cdf4
kw <yimkawaii@gmail.com>
2022-05-11 19:35:18 -0400

    added 2 mqs

--- src/main.js
+++ src/main.js
@@ -7,3 +7,33 @@ function makeMarquee() {
 
 makeMarquee();
 
+
+var myStringArray = ["https://www.linkedin.com/in/kawaii","https://github.com/yimkawai", "https://twitter.com/devignery", "https://instagram.com/aboar"]; 
+var myStringArray2 = ["linkedin", "github", "twitter", "instagram"];
+var myStringArray3 = ["✌︎", "＃", "✎", "ツ"]
+var result = ""
+for (var i = 0; i < myStringArray.length; i++) {
+    result = result + " " + myStringArray3[i] + " " + " <a href='" + myStringArray[i] + "'>" + myStringArray2[i] + "</a>";
+}
+
+function makeMarquee2() {
+    const info = result;
+    const mq = new Array(150).fill(info).join(' ');
+    document.getElementById('mqInfo').innerHTML = mq
+
+}
+
+makeMarquee2();
+
+
+var mail = document.createElement("a");
+mail.href = "yimkawaii@gmail.com";
+mail.innerHTML = "✉ yimkawaii@gmail.com";
+
+function makeMarquee3() {
+    const info = mail.innerHTML;
+    const mq = new Array(150).fill(info).join(' ');
+    document.getElementById('mqEmail').innerHTML = mq;
+}
+
+makeMarquee3();
